bb6c71191c1755fd8835a8331992375b29252cb1
malvaceae <malvaceae@hotmail.com>
2025-01-12 02:00:00 +0900

    add base files

---
+++ kvsWebrtcClientMasterGst.c
@@ -0,0 +1,8 @@
+#include <gst/gst.h>
+#include <gst/app/gstappsink.h>
+#include <com/amazonaws/kinesis/video/webrtcclient/Include.h>
+
+INT32 main(INT32 argc, CHAR* argv[])
+{
+  return EXIT_SUCCESS;
+}
